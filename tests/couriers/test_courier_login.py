e3a8c3fc5098ba467a4faae46553e2273249b1a6
Dana Panfilova <panfilova.dana23@gmail.com>
2024-12-08 18:50:11 +0300

    Добавила аллюр-декораторы.

--- tests/couriers/test_courier_login.py
+++ tests/couriers/test_courier_login.py
@@ -1,10 +1,14 @@
 import pytest
+import allure
 
 from data.data import COURIER_DATA_WITHOUT_LOGIN, COURIER_DATA_WITHOUT_PASSWORD
 from methods.courier_methods import CourierMethods
 
 
 class TestCourierLogin:
+
+    @allure.title("Успешный вход курьера с корректными данными")
+    @allure.step("Тестирование входа курьера с корректными данными")
     def test_login_courier_with_correct_data_is_successfull(self, existing_courier):
         courier_login_json = existing_courier.get_login_json()
 
@@ -12,7 +16,8 @@ class TestCourierLogin:
 
         assert status_code == 200 and json['id'], f'Info: status code: {status_code}, json: {json}, courier_login_json: {courier_login_json}'
 
-
+    @allure.title("Вход с некорректными данными")
+    @allure.step("Тестирование входа с некорректными данными")
     def test_login_with_incorrect_data(self, existing_courier):
         incorrect_login_data = {
             'login': existing_courier.login,
@@ -26,14 +31,18 @@ class TestCourierLogin:
         "login_courier_data",
         [
             COURIER_DATA_WITHOUT_LOGIN,
-            # COURIER_DATA_WITHOUT_PASSWORD - почему-то если отдавать только логин, сервис зависает - вероятно ошибка разработчиков.
+            #COURIER_DATA_WITHOUT_PASSWORD - почему-то если отдавать только логин, сервис зависает - вероятно ошибка разработчиков.
         ]
     )
+    @allure.title("Попытка входа в систему курьера без обязательных параметров")
+    @allure.step("Тестирование входа курьера без всех требуемых параметров")
     def test_login_courier_without_all_requirement_parameters_returns_error(self, login_courier_data: dict):
         status_code, json = CourierMethods.login_courier(login_courier_data)
 
         assert status_code == 400 and json['message'] == 'Недостаточно данных для входа', f'Info: status code: {status_code}, json: {json}, login_courier_data: {login_courier_data}'
 
+    @allure.title("Вход с существующим курьером возвращает ID")
+    @allure.step("Тестирование входа с существующим курьером")
     def test_login_with_existing_courier_returns_id(self, existing_courier):
         status_code, json = CourierMethods.login_courier(existing_courier.get_login_json())
 
--- tests/orders/test_create_order.py
+++ tests/orders/test_create_order.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 
 from methods.order_methods import OrderMethods
 
@@ -14,11 +15,15 @@ class TestOrder:
             []
         ]
     )
+    @allure.title("Создание заказа по цвету")
+    @allure.step("Тестирование создания заказа с цветами: {color}")
     def test_create_order_by_color(self, color: list, order_data: dict):
         order_data['color'] = color
         status_code, json = OrderMethods.create_order(order_data)
         assert status_code == 201 and json['track'], f'Info: status code: {status_code}, json: {json}, color: {color}'
 
+    @allure.title("Получение списка заказов")
+    @allure.step("Тестирование получения списка заказов")
     def test_list_of_orders_getting(self):
         status_code, json = OrderMethods.get_all_orders(1)
         assert status_code == 200 and json['orders'], f'Info: status code: {status_code}, json: {json}'
